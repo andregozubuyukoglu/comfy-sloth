6637daf74b26ed1d3c703ea2c8fec078b85c3eda
Andre <andregozubuyuk@gmail.com>
2023-11-21 23:00:46 +0300

    Updated files

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,16 +1,14 @@
 import React from "react"
-import { Navigate } from "react-router-dom"
 import { useAuth0 } from "@auth0/auth0-react"
-import { useUserContext } from "../context/user_context"
 import { Redirect, Route } from "react-router-dom/cjs/react-router-dom.min"
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext()
+  const { user } = useAuth0()
   return (
     <Route
       {...rest}
       render={() => {
-        return myUser ? children : <Redirect to="/"></Redirect>
+        return user ? children : <Redirect to="/"></Redirect>
       }}
     ></Route>
   )
